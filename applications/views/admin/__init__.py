2c8eae60fa46f3bf76e274483d7b5bd37200ec08
mkg <1650473152@qq.com>
2021-02-23 10:49:04 +0800

    更新sql文件（图片上传）

--- applications/views/admin/__init__.py
+++ applications/views/admin/__init__.py
@@ -1,7 +1,7 @@
 from applications.views.admin.index import admin_index
 from applications.views.admin.user import admin_user
 from applications.views.admin.file import admin_file
-
+from applications.views.admin.monitor import admin_Monitor
 def init_adminViews(app):
     """
     初始化蓝图
@@ -11,3 +11,4 @@ def init_adminViews(app):
     app.register_blueprint(admin_index)
     app.register_blueprint(admin_user)
     app.register_blueprint(admin_file)
+    app.register_blueprint(admin_Monitor)
--- pear.sql
+++ pear.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 15/02/2021 18:58:56
+ Date: 23/02/2021 10:47:07
 */
 
 SET NAMES utf8mb4;
@@ -29,7 +29,28 @@ CREATE TABLE `alembic_version`  (
 -- ----------------------------
 -- Records of alembic_version
 -- ----------------------------
-INSERT INTO `alembic_version` VALUES ('23078ba5f01f');
+INSERT INTO `alembic_version` VALUES ('f1f04ea373ac');
+
+-- ----------------------------
+-- Table structure for photo
+-- ----------------------------
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE `photo`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mime` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `size` char(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `href` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of photo
+-- ----------------------------
+INSERT INTO `photo` VALUES (30, 'image/png', '2204', 'http://127.0.0.1:5000/_uploads/photos/6958819_pear-admin_1607443454.png', '2021-02-20 14:56:10', '6958819_pear-admin_1607443454.png');
+INSERT INTO `photo` VALUES (31, 'image/png', '20317', 'http://127.0.0.1:5000/_uploads/photos/5265086_ncstmkg_1594316803.png', '2021-02-21 11:17:12', '5265086_ncstmkg_1594316803.png');
+INSERT INTO `photo` VALUES (33, 'image/jpeg', '1000979', 'http://127.0.0.1:5000/_uploads/photos/123123123.jpg', '2021-02-21 12:03:26', '123123123.jpg');
 
 -- ----------------------------
 -- Table structure for user
@@ -44,12 +65,12 @@ CREATE TABLE `user`  (
   `create_at` datetime(0) NULL DEFAULT NULL,
   `update_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, NULL, NULL, '普通用户', NULL, NULL, NULL);
-INSERT INTO `user` VALUES (3, 'admin', 'pbkdf2:sha256:150000$OIjfUo6h$1566901cd7e1cfb19110fa545b7d2417fa6e0c7dc53f701e972952b2e8eb7be2', '管理员', 1, NULL, NULL);
+INSERT INTO `user` VALUES (5, 'mkg', 'pbkdf2:sha256:150000$8G9zYhrz$3ace8e0c0852265135bed38298929be39f88eb39d741210cda3ba20fe32a560b', '管理员', 1, '2021-02-16 01:10:13', '2021-02-16 01:10:13');
+INSERT INTO `user` VALUES (6, 'admin', 'pbkdf2:sha256:150000$m6SlqCGu$c3ad0cd18ff7c3a8303b909177ac5d2216b79981e630f6c19489860e752611c0', '管理员', 1, '2021-02-16 01:10:54', '2021-02-16 01:10:54');
 
 SET FOREIGN_KEY_CHECKS = 1;

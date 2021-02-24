d29854af78ba656228906d1dec6545754db4282b
mkg <1650473152@qq.com>
2021-02-24 18:48:27 +0800

    解决图片上传预览超过屏幕的响应式问题

--- applications/views/admin/monitor.py
+++ applications/views/admin/monitor.py
@@ -1,18 +1,10 @@
-import os
-
 from flask import Blueprint, request, render_template, jsonify, current_app
 from flask_marshmallow import Marshmallow
-from marshmallow import fields
-from sqlalchemy import desc
-
-from applications.models import db
-from applications.models.admin import Photo
-from applications.service.upload import photos
 
 ma = Marshmallow()
 admin_Monitor = Blueprint('adminMonitor', __name__, url_prefix='/admin/monitor')
 
+
 @admin_Monitor.route('/')
 def index():
     return render_template('admin/monitor.html')
-

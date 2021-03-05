54d8ea74b1c5844848b24aac8109301a8b362602
mkg <1650473152@qq.com>
2021-03-05 23:47:46 +0800

    initDb.py中PODT强制int类型

--- dev/initDb.py
+++ dev/initDb.py
@@ -4,7 +4,7 @@ from applications.config.database import HOST, USERNAME, PASSWORD, DATABASE,PORT
 
 
 def is_exist_database():
-    db = pymysql.connect(host=HOST,port = PORT, user=USERNAME, password=PASSWORD, charset='utf8mb4')
+    db = pymysql.connect(host=HOST,port = int(PORT), user=USERNAME, password=PASSWORD, charset='utf8mb4')
     cursor1 = db.cursor()
     sql = "select * from information_schema.SCHEMATA WHERE SCHEMA_NAME = '%s'  ; " % DATABASE
     res = cursor1.execute(sql)
@@ -13,7 +13,7 @@ def is_exist_database():
 
 
 def init_database():
-    db = pymysql.connect(host=HOST,port = PORT, user=USERNAME, password=PASSWORD, charset='utf8mb4')
+    db = pymysql.connect(host=HOST,port = int(PORT), user=USERNAME, password=PASSWORD, charset='utf8mb4')
     cursor1 = db.cursor()
     sql = "CREATE DATABASE IF NOT EXISTS %s" % DATABASE
     res = cursor1.execute(sql)

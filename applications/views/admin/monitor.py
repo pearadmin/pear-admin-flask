f0516936907c99feab0fa3cce2d611cdd5ec7f09
mkg <1650473152@qq.com>
2021-05-29 13:00:55 +0800

    完成主机监控动态图表

--- applications/views/admin/monitor.py
+++ applications/views/admin/monitor.py
@@ -2,12 +2,12 @@ import os
 import platform
 import re
 from datetime import datetime
-
+import time
 import psutil
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from flask_marshmallow import Marshmallow
 
-from applications.service.route_auth import authorize_and_log
+from applications.service.route_auth import authorize_and_log, authorize
 
 ma = Marshmallow()
 admin_Monitor = Blueprint('adminMonitor', __name__, url_prefix='/admin/monitor')
@@ -58,6 +58,8 @@ def main():
     up_time_list = re.split(r':', str(up_time))
     up_time_format = " {} 小时{} 分钟{} 秒".format(up_time_list[0], up_time_list[1], up_time_list[2])
 
+    # 当前时间
+    time_now = time.strftime('%H:%M:%S ', time.localtime(time.time()))
     return render_template('admin/monitor.html',
                            hostname=hostname,
                            system_version=system_version,
@@ -70,5 +72,20 @@ def main():
                            memory_free=memory_free,
                            boot_time=boot_time,
                            up_time_format=up_time_format,
-                           disk_partitions_list=disk_partitions_list
+                           disk_partitions_list=disk_partitions_list,
+                           time_now=time_now
+
                            )
+
+
+# 图表api
+@admin_Monitor.route('/polling')
+@authorize("admin:monitor:main")
+def ajax_polling():
+    # 获取cup使用率
+    cpus_percent = psutil.cpu_percent(interval=0.1)
+    # 获取内存使用率
+    memory_information = psutil.virtual_memory()
+    memory_usage = memory_information.percent
+    time_now = time.strftime('%H:%M:%S ', time.localtime(time.time()))
+    return jsonify(cups_percent=cpus_percent, memory_used=memory_usage, time_now=time_now)

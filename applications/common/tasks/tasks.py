48eebc1c95633c976d12b05ebc4784aea03ba71b
mkg <1650473152@qq.com>
2021-08-26 15:31:36 +0800

    定时任务（部分）

--- applications/common/tasks/tasks.py
+++ applications/common/tasks/tasks.py
@@ -1,42 +1,6 @@
 import datetime
-from flask import current_app
 
-from applications.extensions import db
-from applications.extensions.init_apscheduler import scheduler
-from applications.models import Dept
-
-
-# @scheduler.task(
-#     "interval",
-#     id="job_sync",
-#     seconds=10,
-#     max_instances=1,
-#     start_date="2000-01-01 12:19:00",
-# )
-# def task1():
-#     # oh, do you need something from config?
-#     with scheduler.app.app_context():
-#         print(scheduler.app.config)  # noqa: T001
-#         address = "ces"
-#         deptName = "测试"
-#         email = "123@qq.com"
-#         leader = "测试的"
-#         parentId = "1"
-#         phone = "123456"
-#         sort = "5"
-#         status = 1
-#         dept = Dept(
-#             parent_id=parentId,
-#             dept_name=deptName,
-#             sort=sort,
-#             leader=leader,
-#             phone=phone,
-#             email=email,
-#             status=status,
-#             address=address
-#         )
-#         r = db.session.add(dept)
-#         db.session.commit()
+task_list = ['task2', 'task3', 'task4']
 
 
 def task2(a, b):
--- applications/view/admin/task.py
+++ applications/view/admin/task.py
@@ -1,29 +1,16 @@
 from flask import Blueprint, request, render_template
 from flask_apscheduler.utils import job_to_dict
 
-from applications.common.tasks.tasks import task2, task3, task4
+from applications.common.tasks import tasks
+from applications.common.tasks.tasks import task_list
 from applications.common.utils.http import table_api, fail_api, success_api
 from applications.extensions.init_apscheduler import scheduler
 
 admin_task = Blueprint('adminTask', __name__, url_prefix='/admin/task')
 
 
-# 暂停任务
-# scheduler.pause_job('third')
-# 恢复任务
-# time.sleep(10)
-# scheduler.resume_job('third')
-# 删除任务
-# scheduler.remove_job('first')
-
-
 @admin_task.route('/add_job', methods=['GET'])
 def add_task():
-    tasks = {
-        "task2": task2,
-        "task3":task3,
-        "task4":task4
-    }
     scheduler.add_job(func=tasks.get(), id='4', args=(1, 1), trigger='interval', seconds=3,
                       replace_existing=True)
     return '6'
@@ -40,22 +27,61 @@ def get_task():  # 获取
     jobs_list = []
     for job in jobs:
         jobs_list.append(job_to_dict(job))
-    return table_api(data=jobs_list)
+    return table_api(data=jobs_list, count=len(jobs_list))
 
 
 # 增加
 @admin_task.get('/add')
 def add():
-    return render_template('admin/task/add.html')
+    return render_template('admin/task/add.html', task_list=task_list)
+
+
+@admin_task.post('/save')
+def save():
+    _id = request.json.get("id")
+    name = request.json.get("id")
+    type = request.json.get("type")
+    functions = request.json.get("functions")
+    datetime = request.json.get("datetime")
+    time = request.json.get("time")
+    if not hasattr(tasks, functions):
+        return fail_api()
+    if type == 'date':
+        scheduler.add_job(
+            func=getattr(tasks, functions),
+            id=_id,
+            name=name,
+            args=(1, 1),
+            trigger=type,
+            run_date=datetime,
+            replace_existing=True)
+    elif type == 'interval':
+        scheduler.add_job(
+            func=getattr(tasks, functions),
+            id=_id,
+            name=name,
+            args=(1, 1),
+            trigger=type,
+            replace_existing=True)
+    elif type == 'cron':
+        scheduler.add_job(
+            func=getattr(tasks, functions),
+            id=_id,
+            name=name,
+            args=(1, 1),
+            trigger=type,
+            replace_existing=True)
+
+    return success_api()
 
 
 # 恢复
 @admin_task.put('/enable')
 def enable():
-    id = request.json.get('id')
+    _id = request.json.get('id')
     # print(id)
-    if id:
-        scheduler.resume_job(str(id))
+    if _id:
+        scheduler.resume_job(str(_id))
         return success_api(msg="启动成功")
     return fail_api(msg="数据错误")
 
@@ -74,8 +100,6 @@ def dis_enable():
 def remove_job(_id):  # 移除
     scheduler.remove_job(str(_id))
     return success_api(msg="删除成功")
-
-
 
     #     scheduler.add_job(func=task1, id='2', args=(1, 1), trigger='cron', day_of_week='0-6', hour=18, minute=24,
     #                       second=10, replace_existing=True)
